32c06d589d108bc13a54918af01604265282c787
Suliman alharbi <alharbicode@gmail.com>
2026-05-26 23:46:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+console.log("JS WORKING ✅");
+
+const gift = document.getElementById("gift");
+const openBtn = document.getElementById("openBtn");
+const nameInput = document.getElementById("nameInput");
+const intro = document.getElementById("intro");
+const card = document.getElementById("card");
+const personName = document.getElementById("personName");
+const shareBtn = document.getElementById("shareBtn");
+
+let opened = false;
+
+function openGift(){
+
+  const name = nameInput.value.trim();
+
+  if(!name){
+    alert("اكتب اسمك أولًا ✨");
+    nameInput.focus();
+    return;
+  }
+
+  if(opened) return;
+
+  opened = true;
+
+  gift.classList.add("open");
+
+  setTimeout(() => {
+
+    intro.style.display = "none";
+
+    card.style.display = "flex";
+
+    personName.textContent = name;
+
+  }, 900);
+}
+
+openBtn.addEventListener("click", openGift);
+
+gift.addEventListener("click", openGift);
+
+shareBtn.addEventListener("click", () => {
+
+  const senderName = personName.textContent;
+
+  const text =
+`🎁 هدية عيد رقمية
+
+${senderName} أرسل لك تهنئة خاصة بعيد الأضحى المبارك ✨
+
+افتح الرابط واكتب اسمك لتستلم هديتك أنت أيضًا 🌙
+
+تقبل الله منا ومنكم صالح الأعمال
+وكل عام وأنتم بخير
+
+صُممت بواسطة:
+م. سليمان الحربي · alharbi code`;
+
+  window.open(
+    "https://wa.me/?text=" + encodeURIComponent(text),
+    "_blank"
+  );
+});
